0f1f770021f3676de6c381884c92f145140b2b1c
yandif <me.yandif@gmail.com>
2023-08-06 23:56:41 +0800

    refactor: remove example

--- example/src/DataSet/Request.ts
+++
@@ -1,2 +0,0 @@
-export type RequestMethods = {};
-export type RequestConfig = {};
--- example/src/DataSet/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './DataSet';
-export * from './Field';
-export * from './Record';
-export * from './Request';
-export * from './useDataSet';
