f0fe1caa97e272bd63bbba5536e79c72a13352d1
yandif <me.yandif@gmail.com>
2023-08-15 14:47:11 +0800

    feat: add kitten-dataset

---
+++ packages/kitten-dataset/vite.config.ts
@@ -0,0 +1,34 @@
+import { defineConfig, splitVendorChunkPlugin } from 'vite';
+import dts from 'vite-plugin-dts';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  esbuild: {
+    jsx: 'transform',
+  },
+  build: {
+    target: 'es2015',
+    lib: {
+      name: 'kittenDateset',
+      entry: './src/index.ts',
+      fileName: 'index',
+    },
+    rollupOptions: {
+      external: ['react'],
+      output: {
+        globals: { react: 'React' },
+      },
+    },
+  },
+  plugins: [
+    dts({
+      staticImport: true,
+      insertTypesEntry: true,
+      cleanVueFileName: true,
+      copyDtsFiles: false,
+      // rollupTypes: true,
+      outDir: 'lib',
+    }),
+    splitVendorChunkPlugin(),
+  ],
+});
