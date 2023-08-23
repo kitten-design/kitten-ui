b28eb7c930746bea2701c7ecfbd06bec52549a1b
yandif <me.yandif@gmail.com>
2023-08-23 16:29:37 +0800

    fix: Remove unused imports

--- packages/kitten-core/src/components/Transition/Transition.style.ts
+++ packages/kitten-core/src/components/Transition/Transition.style.ts
@@ -1,5 +1,4 @@
 import type { Property } from '@kitten-ui/styles/types/csstype';
-import type React from 'react';
 import type { CSSProperties } from 'react';
 
 import type { KittenTransition } from './transitions';
