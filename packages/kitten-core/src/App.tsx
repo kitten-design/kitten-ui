e99a4c5c24c5a54a66d552229b40666a8dfb13c9
yandif <me.yandif@gmail.com>
2023-09-08 19:30:57 +0800

    docs: remove demo

--- packages/kitten-core/src/App.tsx
+++ packages/kitten-core/src/App.tsx
@@ -1,41 +1,15 @@
 import React from 'react';
 
-import { Box, Button, Flex, toast, Toaster, Tooltip } from './';
-let i = 0;
+import { Box, Button } from './';
+
 const App = () => {
   const [theme, setTheme] = React.useState('light');
 
   return (
     <Box className={theme} w="100vw" h="100vh" overflow={'auto'} styled>
-      <Tooltip
-        label={
-          ' undefined undefinedundefinundefined undefinedundefinundefined undefinedundefinundefined undefinedundefinundefined undefinedundefinundefined undefinedundefinundefined undefinedundefined undefined'
-        }>
-        <Button onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')}>
-          Toggle theme
-        </Button>
-      </Tooltip>
-
-      <Button
-        onClick={() => {
-          i++;
-          toast({
-            message: `toast ${i}`,
-            position: 'bottom-right',
-            autoClose: false,
-          });
-        }}>
-        toast
+      <Button onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')}>
+        Toggle theme
       </Button>
-      <Flex alignItems={'center'} justifyContent={'center'} gap="xl">
-        <Button>按钮</Button>
-        <Button>按钮</Button>
-        <Button>按钮</Button>
-        <Button>按钮</Button>
-        <Button>按钮</Button>
-        <Button>按钮</Button>
-      </Flex>
-      <Toaster className={theme} />
     </Box>
   );
 };
